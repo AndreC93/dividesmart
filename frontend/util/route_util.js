ab986147008d6f4d28d3d36322906e0f8bcde34f
Andre <fendred@yahoo.com>
2018-09-12 13:44:45 -0400

    Add AuthRoute

---
+++ frontend/util/route_util.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const Auth = ({component: Component, path, loggedIn, exact}) => (
+  <Route path={path} exact={exact} render={(props) => {
+    if (!loggedIn)  {
+      return (<Component {...props} />);
+    } else {
+      return (<Redirect to="/" />);
+    }
+  } } />
+);
+
+const mapStateToProps = state => {
+  return {loggedIn: Boolean(state.session.currentUserId)};
+};
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
