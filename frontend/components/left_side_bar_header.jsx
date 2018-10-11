55da787c15c984df886cfb4d8784014dd4f5f2b4
Andre <fendred@yahoo.com>
2018-10-11 15:36:21 -0400

    Fix dashboard icons bug

--- frontend/components/left_side_bar_header.jsx
+++ frontend/components/left_side_bar_header.jsx
@@ -7,23 +7,27 @@ class LeftSideBarHeader extends React.Component {
       borderLeft: '6px solid #5bc5a7',
       color: '#5bc5a7',
     };
-    let imageHighlight = {};
+    let imageHighlight = {
+      background: `url(${window.dashboardIcons}) 0 0`,
+      backgroundRepeat: 'norepeat',
+    };
     let highlight1 = {};
     let highlight2 = {};
     let highlight3 = {};
     if ((this.props.location.pathname) === '/dashboard') {
       highlight1 = highlight;
-      imageHighlight = { backgroundPositionX: '0' };
+      imageHighlight.backgroundPositionX = '0';
     } else if ((this.props.location.pathname) === '/activity') {
       highlight2 = highlight;
     } else if ((this.props.location.pathname) === '/all') {
       highlight3 = highlight;
     }
+
     return (
       <div className='left-side-bar-header' >
         <li style={ highlight1 } onClick={ () => (
             this.props.history.push('/dashboard')) } >
-          <i style={ imageHighlight } />
+          <i id='dashIcons' style={ imageHighlight } />
           <a> Dashboard</a>
         </li>
         <li style={ highlight2 } onClick={ () => (
